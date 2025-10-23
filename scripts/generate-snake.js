d8b241ea37217d268d9603d18df0f680dd29d40b
Marcio cley <162837149+marciocleydev@users.noreply.github.com>
2025-10-23 18:00:34 -0300

    Change theme to 'rocket' in generate-snake.js

--- scripts/generate-snake.js
+++ scripts/generate-snake.js
@@ -4,7 +4,7 @@ import { get } from 'https';
 // CONFIGURAÇÕES - MUDE AQUI PARA PERSONALIZAR
 const CONFIG = {
   username: 'marciocleydev',
-  theme: 'pacman', // 'snake', 'rocket', 'pacman', 'dinosaur'
+  theme: 'rocket', // 'snake', 'rocket', 'pacman', 'dinosaur'
   colors: {
     background: '#ebedf0',
     grid: ['#9be9a8', '#40c463', '#30a14e', '#216e39'],
